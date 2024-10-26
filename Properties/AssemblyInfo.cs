72541c167ad0e2649a7b0bf2274b9486965f6177
doge15567 <41397214+doge15567@users.noreply.github.com>
2024-10-26 11:54:28 -0500

    Fix AssemblyInfo
    Prevent auto-generation of assembly info file

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,6 +10,14 @@ using MelonLoader;
 [assembly: MelonInfo(typeof(FusionIntermediateServerHelper.Main), FusionIntermediateServerHelper.Main.Name, FusionIntermediateServerHelper.Main.Version, FusionIntermediateServerHelper.Main.Author, FusionIntermediateServerHelper.Main.DownloadLink)]
 [assembly: MelonColor(50, 168, 82, 255)]
 
+[assembly: System.Reflection.AssemblyCompanyAttribute("FusionIntermediateServerHelper")]
+//[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute(FusionIntermediateServerHelper.Main.Version)]
+[assembly: System.Reflection.AssemblyProductAttribute("FusionIntermediateServerHelper")]
+[assembly: System.Reflection.AssemblyTitleAttribute("FusionIntermediateServerHelper")]
+
+
+
 // Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
 // If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
 // Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
